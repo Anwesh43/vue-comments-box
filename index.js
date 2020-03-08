4ace739696d2ad02c811b34697e7dc6f46ad50b7
Anwesh43 <anweshthecool0@gmail.com>
2020-03-09 00:22:06 +0530

    adding scale fix

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const w = window.innerWidth, h = window.innerHeight, sizeFactor = 10, textFactor = 20
-const scGap = 0.02
+const w = window.innerWidth, h = window.innerHeight, sizeFactor = 4, textFactor = 20
+const scGap = 0.05
 
-Vue.component('CommentBox', {
-    props : ['scale', 'text'],
-    data() {
-        const objStyle = {
-            width : `${(Math.min(w, h) * this.scale) / sizeFactor}px`,
-            height : `${(Math.min(w, h) * this.scale) / sizeFactor}px`,
-            background : '#3F51B5',
-            WebkitTransform : `rotate(${360 * this.scale}deg)`,
-            color : 'white',
-            fontSize : `${Math.min(w, h) / textFactor}`,
-            textAlign : 'center'
+Vue.component('comment', {
+    props : ['text', 'i', 'scale'],
+    computed : {
+        objStyle() {
+          console.log(this.scale)
+          const style = {
+              width : `${(Math.min(w, h) * this.scale) / sizeFactor}px`,
+              height : `${(Math.min(w, h) * this.scale) / sizeFactor}px`,
+              background : '#3F51B5',
+              WebkitTransform : `rotate(${360 * this.scale}deg)`,
+              color : 'white',
+              fontSize : `${Math.min(w, h) / textFactor}px`,
+              textAlign : 'center',
+              float : 'top',
+              margin : `${Math.min(w, h) / textFactor}px`
+          }
+          return style
         }
     },
     template : '<div :style = "objStyle">{{text}}</div>'
@@ -22,7 +28,7 @@ class Animator {
     start(cb) {
         if (!this.animated) {
             this.animated = true
-            this.interval = setInterval(cb, 50)
+            this.interval = setInterval(cb, 30)
         }
     }
 
@@ -65,16 +71,38 @@ const app = new Vue({
     data : {
         currText : '',
         comments : [],
-        btnDisabled: false
+        btnDisabled: false,
+        animator : new Animator(),
+        currScale : 0
     },
     methods : {
         add() {
-            this.comments.push(this.currText)
+            const text = this.currText
+            const i = this.comments.length
+            const comment = {text, i, scale : 0}
+            this.comments.push(comment)
             this.btnDisabled = true
-            setTimeout(() => {
-                this.currText = ''
-                this.btnDisabled = false
-            }, 5000)
+            const state = new State()
+            state.startUpdating(() => {
+                this.animator.start(() => {
+                  //console.log(state.scale)
+                    this.currScale = state.scale
+                    this.comments[i].scale = this.currScale
+                    state.update(() => {
+                        this.animator.stop()
+                        this.currText = ''
+                        this.btnDisabled = false
+                        this.comments[i].scale = state.scale
+                    })
+                })
+            })
+        },
+        setScale(i) {
+            if (i == this.comments.length - 1) {
+                console.log(this.currScale)
+
+
+            }
         }
     },
     computed : {
